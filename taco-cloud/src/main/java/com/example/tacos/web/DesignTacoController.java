a91ec7cad784df1d272b3d8ae52b13af21f97649
Konradek <konradbiznesu@gmail.com>
2020-11-08 20:50:50 +0100

    Sql schema added

--- taco-cloud/src/main/java/com/example/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/com/example/tacos/web/DesignTacoController.java
@@ -12,6 +12,7 @@ import com.example.tacos.Ingredient;
 import com.example.tacos.Ingredient.Type;
 import com.example.tacos.Taco;
 import com.example.tacos.data.IngredientRepository;
+import com.example.tacos.data.TacoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,33 +31,19 @@ import lombok.extern.slf4j.Slf4j;
 public class DesignTacoController {
 
     private final IngredientRepository ingredientRepo;
+    private TacoRepository designRepo;
 
     @Autowired
     public DesignTacoController(IngredientRepository ingredientRepo){
         this.ingredientRepo = ingredientRepo;
     }
 
-    @ModelAttribute
-    public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Pszenna", Type.WRAP),
-                new Ingredient("COTO", "Kukurydzian", Type.WRAP),
-                new Ingredient("GRBF", "Mielona wołowina", Type.PROTEIN),
-                new Ingredient("CARN", "Kawałki mięsa", Type.PROTEIN),
-                new Ingredient("TMTO", "Pomidory", Type.VEGGIES),
-                new Ingredient("LETC", "Sałatka", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Śmietana", Type.SAUCE)
-        );
-
-        Type[] types = Ingredient.Type.values();
-        for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(),
-                    filterByType(ingredients, type));
-        }
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
     }
+
+
     @GetMapping
     public String showDesignForm(Model model) {
         List<Ingredient> ingredients = new ArrayList<>();

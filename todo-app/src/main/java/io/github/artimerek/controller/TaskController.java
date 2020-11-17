c4ad1b6f236de6d909743ad646172a717a09489e
Konradek <konradbiznesu@gmail.com>
2020-11-17 17:56:03 +0100

    GetMappings

--- todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
+++ todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
@@ -3,7 +3,10 @@ package io.github.artimerek.controller;
 import io.github.artimerek.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RepositoryRestController
 public class TaskController {
@@ -14,4 +17,14 @@ public class TaskController {
         this.repository = repository;
     }
 
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    ResponseEntity<?> readAllTasks() {
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(repository.findAll());
+    }
+    @GetMapping(value = "/tasks")
+    ResponseEntity<?> readAllTasks(Pageable page) {
+        logger.info("Custom pager");
+        return ResponseEntity.ok(repository.findAll(page));
+    }
 }

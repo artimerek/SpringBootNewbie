fafa1f5ff4342f32ab6616cd9fe416eef49e058b
Konradek <konradbiznesu@gmail.com>
2020-11-17 18:05:14 +0100

    Refactoring

--- todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
+++ todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
@@ -1,14 +1,17 @@
 package io.github.artimerek.controller;
 
+import io.github.artimerek.model.Task;
 import io.github.artimerek.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RepositoryRestController
+import java.util.List;
+
+@RestController
 public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -18,13 +21,13 @@ public class TaskController {
     }
 
     @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
-    ResponseEntity<?> readAllTasks() {
+    ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
     @GetMapping(value = "/tasks")
-    ResponseEntity<?> readAllTasks(Pageable page) {
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pager");
-        return ResponseEntity.ok(repository.findAll(page));
+        return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 }

c7b6ec39215beb5c2bc43136b5df9b5d8879a567
Konradek <konradbiznesu@gmail.com>
2020-11-17 21:46:44 +0100

    Update task putmapping method

--- todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
+++ todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,9 +28,15 @@ public class TaskController {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
-    @GetMapping(value = "/tasks")
+    @GetMapping("/tasks")
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?>updateTask(@RequestBody Task toUpdate){
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
     }
 }

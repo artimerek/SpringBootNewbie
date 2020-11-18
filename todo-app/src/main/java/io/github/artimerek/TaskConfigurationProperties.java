28a0359dc111f09bf2c4de21ade0b146064fdd12
Konradek <konradbiznesu@gmail.com>
2020-11-18 13:51:15 +0100

    info controller, my own property and switching off spring banner3

--- todo-app/src/main/java/io/github/artimerek/TaskConfigurationProperties.java
+++ todo-app/src/main/java/io/github/artimerek/TaskConfigurationProperties.java
@@ -1,2 +1,18 @@
-package io.github.artimerek;public class TaskConfigurationProperties {
+package io.github.artimerek;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("task")
+public class TaskConfigurationProperties {
+    private boolean allowMultipleTasksFromTemplate;
+
+    public boolean isAllowMultipleTasksFromTemplate() {
+        return allowMultipleTasksFromTemplate;
+    }
+
+    public void setAllowMultipleTasksFromTemplate(boolean allowMultipleTasksFromTemplate) {
+        this.allowMultipleTasksFromTemplate = allowMultipleTasksFromTemplate;
+    }
 }

1182b298549597904c28fed5df36912791b81d5f
Konradek <konradbiznesu@gmail.com>
2020-11-18 11:54:24 +0100

    app properties

--- todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
+++ todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
@@ -1,7 +1,6 @@
 package io.github.artimerek.controller;
 
 import io.github.artimerek.model.Task;
-import io.github.artimerek.model.SqlTaskRepository;
 import io.github.artimerek.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

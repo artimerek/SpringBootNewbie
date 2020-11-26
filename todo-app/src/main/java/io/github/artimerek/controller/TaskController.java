b909b434598bd05563272c39aa8a1c753ac574e4
Konradek <konradbiznesu@gmail.com>
2020-11-26 22:04:45 +0100

    @Transactional

--- todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
+++ todo-app/src/main/java/io/github/artimerek/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -33,12 +34,22 @@ public class TaskController {
     }
 
     @PutMapping("/tasks/{id}")
-    ResponseEntity<?>updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate){
+    ResponseEntity<?>toggleTask(@PathVariable int id, @RequestBody @Valid Task toUpdate){
         if(!repository.existsById(id)){
             return ResponseEntity.notFound().build();
         }
         toUpdate.setId(id);
         repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
+    }
+    @Transactional
+    @PatchMapping("/tasks/{id}")
+    public ResponseEntity<?>toggleTask(@PathVariable int id){
+        if(!repository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        repository.findById(id)
+                .ifPresent(task -> task.setDone(!task.isDone()));
         return ResponseEntity.noContent().build();
     }
 

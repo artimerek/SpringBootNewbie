118029f2924a11ff2514ed096c88694157e386b7
Konradek <konradbiznesu@gmail.com>
2020-12-02 22:59:01 +0100

    Project class and migration

--- todo-app/src/main/java/io/github/artimerek/model/ProjectStep.java
+++ todo-app/src/main/java/io/github/artimerek/model/ProjectStep.java
@@ -1,36 +1,26 @@
 package io.github.artimerek.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import java.util.Set;
 
 @Entity
-@Table(name = "task_groups")
-public class TaskGroup {
+@Table(name = "project_steps")
+public class ProjectStep {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NotBlank(message = "Task group's description must not be empty")
+    @NotBlank(message = "Project step's description must not be empty")
     private String description;
-    private boolean done;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group")
-    private Set<Task> tasks;
+    private int daysToDeadline;
     @ManyToOne
     @JoinColumn(name = "project_id")
     private Project project;
-
-    public TaskGroup() {
-    }
 
     public int getId() {
         return id;
@@ -44,24 +34,16 @@ public class TaskGroup {
         return description;
     }
 
-    public void setDescription(final String description) {
+    void setDescription(final String description) {
         this.description = description;
     }
 
-    public boolean isDone() {
-        return done;
+    public int getDaysToDeadline() {
+        return daysToDeadline;
     }
 
-    public void setDone(final boolean done) {
-        this.done = done;
-    }
-
-    public Set<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(final Set<Task> tasks) {
-        this.tasks = tasks;
+    void setDaysToDeadline(final int daysToDeadline) {
+        this.daysToDeadline = daysToDeadline;
     }
 
     Project getProject() {

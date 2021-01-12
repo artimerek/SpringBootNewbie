cceb484ee759d67eadc9cb187186883eac1e510f
Konradek <konradbiznesu@gmail.com>
2021-01-12 21:44:23 +0100

    Tests

---
+++ todo-app/src/test/java/io/github/artimerek/logic/ProjectServiceTest.java
@@ -0,0 +1,12 @@
+package io.github.artimerek.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @Test
+    void createGroup() {
+    }
+}

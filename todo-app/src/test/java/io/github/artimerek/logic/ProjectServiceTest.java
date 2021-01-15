ad1cc7a97c2f8354a9e2aea29dbc34d7b4d9e408
Konradek <konradbiznesu@gmail.com>
2021-01-15 20:10:08 +0100

    noMultipleGroupsConfigtest

--- todo-app/src/test/java/io/github/artimerek/logic/ProjectServiceTest.java
+++ todo-app/src/test/java/io/github/artimerek/logic/ProjectServiceTest.java
@@ -1,12 +1,33 @@
 package io.github.artimerek.logic;
 
+import io.github.artimerek.model.TaskGroup;
+import io.github.artimerek.model.TaskGroupRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ProjectServiceTest {
 
     @Test
-    void createGroup() {
+    @DisplayName("should throw IllegalStateException when configured to allow just 1 group and the other undone group exists")
+    void createGroup_noMultipleGroupsConfig_And_undoneGroupExists_throwsIllegalStateException() {
+        //given
+
+        var mockGroupRepository = mock(TaskGroupRepository.class);
+        when(mockGroupRepository.existsByDoneIsFalseAndAndProject_Id(anyInt())).thenReturn(true);
+
+        //when
+
+
+        //then
+        assertTrue(mockGroupRepository.existsByDoneIsFalseAndAndProject_Id(500));
+
     }
-}
+    }

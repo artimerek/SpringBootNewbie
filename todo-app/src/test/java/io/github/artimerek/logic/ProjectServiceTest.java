4ab5c6f356776f393df0eacfc7d8d3ea5e547878
Konradek <konradbiznesu@gmail.com>
2021-01-15 20:43:57 +0100

    throwing exceptions tests

--- todo-app/src/test/java/io/github/artimerek/logic/ProjectServiceTest.java
+++ todo-app/src/test/java/io/github/artimerek/logic/ProjectServiceTest.java
@@ -1,13 +1,16 @@
 package io.github.artimerek.logic;
 
+import io.github.artimerek.TaskConfigurationProperties;
 import io.github.artimerek.model.TaskGroup;
 import io.github.artimerek.model.TaskGroupRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
@@ -19,15 +22,27 @@ class ProjectServiceTest {
     @DisplayName("should throw IllegalStateException when configured to allow just 1 group and the other undone group exists")
     void createGroup_noMultipleGroupsConfig_And_undoneGroupExists_throwsIllegalStateException() {
         //given
-
         var mockGroupRepository = mock(TaskGroupRepository.class);
         when(mockGroupRepository.existsByDoneIsFalseAndAndProject_Id(anyInt())).thenReturn(true);
+        //and
+        var mockTemplate = mock(TaskConfigurationProperties.Template.class);
+        when(mockTemplate.isAllowMultipleTasks()).thenReturn(false);
+        //and
+        var mocConfig = mock(TaskConfigurationProperties.class);
+        when(mocConfig.getTemplate()).thenReturn(mockTemplate);
+        //system under test
+        var toTest = new ProjectService(null, mockGroupRepository, mocConfig);
 
         //when
+        var exception = catchThrowable(() ->toTest.createGroup(LocalDateTime.now(),0));
+        // then
+        assertThat(exception)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("one undone group");
 
 
-        //then
-        assertTrue(mockGroupRepository.existsByDoneIsFalseAndAndProject_Id(500));
+        }
+
 
     }
-    }
+

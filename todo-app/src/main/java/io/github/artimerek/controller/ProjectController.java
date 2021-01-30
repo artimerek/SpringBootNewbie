0d9f522b1f085914d14b0dd24372a3b9fce64eff
Konradek <konradbiznesu@gmail.com>
2021-01-30 22:08:37 +0100

    Form valid

--- todo-app/src/main/java/io/github/artimerek/controller/ProjectController.java
+++ todo-app/src/main/java/io/github/artimerek/controller/ProjectController.java
@@ -1,14 +1,20 @@
 package io.github.artimerek.controller;
 
 import io.github.artimerek.logic.ProjectService;
+import io.github.artimerek.model.Project;
 import io.github.artimerek.model.ProjectStep;
 import io.github.artimerek.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
 //returns templates
 @Controller
 @RequestMapping("/projects")
@@ -27,7 +33,14 @@ public class ProjectController {
     }
 
     @PostMapping
-    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model) {
+    String addProject(
+            @ModelAttribute("project") @Valid ProjectWriteModel current,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if(bindingResult.hasErrors()){
+            return "projects";
+        }
         projectService.save(current);
         model.addAttribute("project", new ProjectWriteModel());
         model.addAttribute("message", "Dodano projekt");
@@ -38,5 +51,10 @@ public class ProjectController {
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectStep());
         return "projects";
+    }
+
+    @ModelAttribute("projects")
+    List<Project> getProjects(){
+        return projectService.readAll();
     }
 }

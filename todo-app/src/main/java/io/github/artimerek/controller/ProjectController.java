43aaa31834110d82550543a78817c6abbd3de807
Konradek <konradbiznesu@gmail.com>
2021-01-30 22:34:18 +0100

    Refreshing while new project

--- todo-app/src/main/java/io/github/artimerek/controller/ProjectController.java
+++ todo-app/src/main/java/io/github/artimerek/controller/ProjectController.java
@@ -4,15 +4,14 @@ import io.github.artimerek.logic.ProjectService;
 import io.github.artimerek.model.Project;
 import io.github.artimerek.model.ProjectStep;
 import io.github.artimerek.model.projection.ProjectWriteModel;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 //returns templates
@@ -43,6 +42,7 @@ public class ProjectController {
         }
         projectService.save(current);
         model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("projects", getProjects());
         model.addAttribute("message", "Dodano projekt");
         return "projects";
     }
@@ -50,6 +50,22 @@ public class ProjectController {
     @PostMapping(params = "addStep")
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline
+    )   {
+        try {
+            projectService.createGroup(deadline, id);
+            model.addAttribute("message", "Dodano grupę!");
+        }catch (IllegalStateException | IllegalArgumentException e) {
+            model.addAttribute("message", "Błąd podczas tworzenia grupy!");
+        }
         return "projects";
     }
 

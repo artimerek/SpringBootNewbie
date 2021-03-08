00201b63fdc4b6624f719b499d9cc44cf8b3b868
Konradek <konradbiznesu@gmail.com>
2021-03-08 16:14:34 +0100

    Fronted

--- todo-app/src/main/java/io/github/artimerek/model/projection/GroupTaskWriteModel.java
+++ todo-app/src/main/java/io/github/artimerek/model/projection/GroupTaskWriteModel.java
@@ -2,11 +2,15 @@ package io.github.artimerek.model.projection;
 
 import io.github.artimerek.model.Task;
 import io.github.artimerek.model.TaskGroup;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 public class GroupTaskWriteModel {
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime deadline;
 
     public String getDescription() {
@@ -29,4 +33,3 @@ public class GroupTaskWriteModel {
         return new Task(description, deadline, group);
     }
 }
-
---
+++ todo-app/src/test/java/io/github/artimerek/model/projection/GroupReadModelTest.java
@@ -0,0 +1,29 @@
+package io.github.artimerek.model.projection;
+
+import io.github.artimerek.model.Task;
+import io.github.artimerek.model.TaskGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupReadModelTest {
+    @Test
+    @DisplayName("should create null deadline for group when no deadlines")
+    void constructor_noDeadlines_createsNullDeadline(){
+        //given
+        var source = new TaskGroup();
+        source.setDescription("foo");
+        source.setTasks(Set.of(new Task("bar",null)));
+
+        //when
+        var result = new GroupReadModel(source);
+
+        //then
+        assertThat(result).hasFieldOrPropertyWithValue("deadline",null);
+    }
+
+}

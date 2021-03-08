ad8c5bf72b6207e1739295437e7addff8d3b3db0
Konradek <konradbiznesu@gmail.com>
2021-03-08 18:56:49 +0100

    ControllerAdvice

--- todo-app/src/main/java/io/github/artimerek/controller/TaskGroupController.java
+++ todo-app/src/main/java/io/github/artimerek/controller/TaskGroupController.java
@@ -89,15 +89,7 @@ class TaskGroupController {
         return ResponseEntity.noContent().build();
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    ResponseEntity<?> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.notFound().build();
-    }
 
-    @ExceptionHandler(IllegalStateException.class)
-    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
 
     @ModelAttribute("groups")
     List<GroupReadModel> getGroups() {

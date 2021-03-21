c8f3381ab36e26886fa0e036a177bac7f0666178
artimerek <konradbiznesu@gmail.com>
2021-03-21 23:32:01 +0100

    Registration impl

--- register/src/main/java/pl/artimerek/register/registration/RegistrationService.java
+++ register/src/main/java/pl/artimerek/register/registration/RegistrationService.java
@@ -1,10 +1,34 @@
 package pl.artimerek.register.registration;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.artimerek.register.appuser.AppUser;
+import pl.artimerek.register.appuser.AppUserRole;
+import pl.artimerek.register.appuser.AppUserService;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.
+                test(request.getEmail());
+
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }

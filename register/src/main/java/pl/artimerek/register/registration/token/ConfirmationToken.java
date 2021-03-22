ea04b3ce317cd51a52ea942210fc6f5dfbfb5c2a
artimerek <konradbiznesu@gmail.com>
2021-03-22 10:35:57 +0100

    Fast fix

--- register/src/main/java/pl/artimerek/register/registration/token/ConfirmationToken.java
+++ register/src/main/java/pl/artimerek/register/registration/token/ConfirmationToken.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@Entity
 public class ConfirmationToken {
 
     @SequenceGenerator(
--- register/src/main/java/pl/artimerek/register/registration/token/ConfirmationTokenRepository.java
+++ register/src/main/java/pl/artimerek/register/registration/token/ConfirmationTokenRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
 
-    Optional<ConfirmationToken> findBYToken(String token);
+    Optional<ConfirmationToken> findByToken(String token);
 }

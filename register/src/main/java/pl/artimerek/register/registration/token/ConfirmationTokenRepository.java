55f23f52ff872a03242eefd7ff633b67c3442ed7
artimerek <konradbiznesu@gmail.com>
2021-03-22 10:22:19 +0100

    Confirmation package

---
+++ register/src/main/java/pl/artimerek/register/registration/token/ConfirmationTokenRepository.java
@@ -0,0 +1,8 @@
+package pl.artimerek.register.registration.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+}
---
+++ register/src/main/java/pl/artimerek/register/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,4 @@
+package pl.artimerek.register.registration.token;
+
+public class ConfirmationTokenService {
+}

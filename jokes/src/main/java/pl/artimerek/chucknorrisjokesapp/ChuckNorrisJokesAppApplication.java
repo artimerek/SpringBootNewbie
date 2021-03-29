e441b475e74291f312e9e8aac82912e791ae01f9
Konradek <konradbiznesu@gmail.com>
2021-03-30 00:45:28 +0200

    jokes app

---
+++ jokes/src/main/java/pl/artimerek/chucknorrisjokesapp/ChuckNorrisJokesAppApplication.java
@@ -0,0 +1,17 @@
+package pl.artimerek.chucknorrisjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.artimerek.chucknorrisjokesapp.service.JokeService;
+
+@SpringBootApplication
+public class ChuckNorrisJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokesAppApplication.class, args);
+
+
+
+    }
+
+}
---
+++ jokes/src/test/java/pl/artimerek/chucknorrisjokesapp/ChuckNorrisJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.artimerek.chucknorrisjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
